3df21ff3858145e68265be49e81f7985e37350a3
saharadoesdev <smsmo@att.net>
2025-08-16 23:25:16 -0500

    add middleware for checking if admin logged in

---
+++ src/middleware.js
@@ -0,0 +1,9 @@
+import { updateSession } from '@/utils/supabase/middleware'
+
+export async function middleware(request) {
+  return await updateSession(request)
+}
+
+export const config = {
+  matcher: ['/admin/:path*'],
+}
