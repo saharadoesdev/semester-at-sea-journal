a58442c8c1b88ce8e9727fc982f5f868f85c87dd
saharadoesdev <smsmo@att.net>
2025-08-17 00:50:38 -0500

    update admin page with create post button and log out button

--- src/app/(admin)/admin/page.js
+++ src/app/(admin)/admin/page.js
@@ -1,13 +1,24 @@
 'use client'
-import { supabase } from '../../../utils/supabase/client'
-import styles from "../page.module.css";
+import Link from 'next/link';
+import styles from "../../page.module.css";
+import { createClient } from "@/utils/supabase/client";
+const supabase = createClient();
 
 export default function AdminPage() {
+  async function handleLogout() {
+    await supabase.auth.signOut();
+    window.location.href = "/login";
+  }
   return (
     <div className={styles.page}>
         <br /><br /><br /><br /><br />
       <h1>Admin</h1>
-      {/* <CounterButton /> */}
+      {/* <h2>Create Post</h2> */}
+      <Link href="/admin/create">Create Post</Link>
+      <br /><br />
+      <Link href="/admin/edit">Edit Post</Link>
+      <br /><br />
+      <button onClick={handleLogout}>Log Out</button>
     </div>
   );
 }
