1891f27b4a0c90d1d921da426f63d9b3a66ab3f3
saharadoesdev <smsmo@att.net>
2025-08-17 17:36:30 -0500

    update homepage to display latest entry

--- src/app/page.js
+++ src/app/page.js
@@ -1,112 +1,35 @@
-// import Image from "next/image";
+import { createClient } from "@/utils/supabase/server";
+import MapDynamicWrapper from "../components/MapDynamicWrapper";
+import JournalArchive from "@/components/JournalArchive";
 import styles from "./page.module.css";
-import MapDynamicWrapper from '../components/MapDynamicWrapper';
-import JournalCard from '../components/JournalCard';
 
-export default function Home() {
-  return (<>
+export default async function Home() {
+  const supabase = await createClient();
+  const { data: posts, error } = await supabase
+    .from("JournalEntries")
+    .select()
+    .order("created_at", { ascending: false })
+    .limit(1); // probably increase later
+
+  return (
+    <>
       <MapDynamicWrapper />
-    <div className={styles.page}>
-      <h1>Welcome to My Voyage!</h1>
-      <p>Explore the world with me as I document my travels on my Semester at Sea journey.</p>
-      <h2>Latest Journal Entries</h2>
-      <div className={styles.journalCards}>
-        {/* dummy JournalCard components will go here */}
+      <div className={styles.page}>
+        <h1>Welcome to My Voyage!</h1>
+        <p>
+          Explore the world with me as I document my travels on my Semester at
+          Sea journey.
+        </p>
+        <h2>Latest Journal Entries</h2>
+
+        <JournalArchive posts={posts || []} />
+
+        {/* <div className={styles.JournalCards}>
+        <JournalCard />
+        <JournalCard />
+        <JournalCard />
+      </div> */}
       </div>
-
-      <div className={styles.JournalCards}>
-        <JournalCard />
-        <JournalCard />
-        <JournalCard />
-      </div>
-
-      {/* <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer> */}
-    </div>
     </>
   );
 }
