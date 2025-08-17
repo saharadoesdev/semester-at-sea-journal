0b364ac34f014b88e2458ccef76edcd87175def1
saharadoesdev <smsmo@att.net>
2025-08-17 15:13:12 -0500

    increase max file upload size

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    serverActions: {
+        bodySizeLimit: '30mb',
+    },
+};
 
 export default nextConfig;
