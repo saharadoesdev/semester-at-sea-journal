b6024624a0f49180cbb523e0d3efe28af4e6aa83
saharadoesdev <smsmo@att.net>
2025-08-17 18:49:33 -0500

    add edit post page

---
+++ src/app/(admin)/admin/edit/[slug]/page.js
@@ -0,0 +1,71 @@
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import PostEditor from "@/components/PostEditor";
+import styles from "@/app/page.module.css";
+
+export default async function EditPostPage({ params }) {
+  const { slug } = params;
+  const supabase = await createClient();
+  const { data: post, error: fetchError } = await supabase
+      .from('JournalEntries')
+      .select('*')
+      .eq('slug', slug)
+      .single();
+
+  if (fetchError || !post) {
+    return <div>Post not found</div>;
+  }
+
+  const handleEditPost = async (formData) => {
+    "use server";
+    const supabase = await createClient();
+    
+    const existingImageUrls = post.image_urls || [];
+    const files = formData.getAll("images");
+    
+    const newImageUrls = [];
+    for (const file of files) {
+        if (file && file.size > 0) {
+        const { data, error } = await supabase.storage
+          .from("images")
+          .upload(`public/${Date.now()}_${file.name}`, file, {
+            contentType: file.type,
+        });
+        if (error) {
+          console.error("Image upload error:", error);
+          continue;
+        }
+        const { data: publicUrlData } = supabase.storage
+        .from("images")
+          .getPublicUrl(data.path);
+          newImageUrls.push(publicUrlData.publicUrl);
+        }
+    }
+    const image_urls = [...existingImageUrls, ...newImageUrls];
+
+    const updatedPost = {
+      title: formData.get("title") || "",
+      slug: formData.get("slug") || "",
+      content: formData.get("content") || "",
+      display_date: formData.get("displayDate") || null,
+      image_urls,
+      tags: formData.getAll("tags"),
+    };
+    console.log(updatedPost);
+    const { data, error } = await supabase
+      .from("JournalEntries")
+      .update([updatedPost])
+      .eq("slug", slug);
+    console.log("Supabase update result:", { data, error });
+    if (!error) {
+      redirect(`/admin`);
+    }
+  };
+
+  return (
+    <div className={styles.page}>
+      <h1>Edit Journal Entry</h1>
+      <PostEditor initialData={post} action={handleEditPost} />
+    </div>
+  );
+}
