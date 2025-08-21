f0d6727fba787579d37abd24c8cb0875b15ece29
saharadoesdev <smsmo@att.net>
2025-08-21 17:01:27 -0500

    add component to display message wall

---
+++ src/components/message-wall/MessageWallDisplay.jsx
@@ -0,0 +1,35 @@
+"use client";
+import MessageCard from './MessageCard';
+import styles from "@/app/page.module.css";
+
+const MessageWallDisplay = (props) => {
+  const messages = props.messages || [];
+  const loading = false;
+
+  return (
+    <>
+      <div className={styles.MessageCards}>
+        {loading ? (
+          <div className="loading-spinner"></div>
+        ) : messages && messages.length > 0 ? (
+          [...messages]
+            .sort((a, b) => new Date(b.created_at) - new Date(a.created_at))    // Display newest first
+            .map((message, index) => (
+              <MessageCard
+                key={message.id}
+                id={message.id}
+                content={message.content}
+                date={message.date}
+                name={message.name}
+                relation={message.relation}
+              />
+            ))
+        ) : (
+          <h2>{"Looks like the messages got lost at sea! 🌊"}</h2>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default MessageWallDisplay;
