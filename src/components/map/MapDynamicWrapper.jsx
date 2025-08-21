482f00dfcb88e92556e05213a28c33a1adf971e7
saharadoesdev <smsmo@att.net>
2025-08-21 16:37:27 -0500

    fix dynamic map wrapper's import of mapcontainer

--- src/components/map/MapDynamicWrapper.jsx
+++ src/components/map/MapDynamicWrapper.jsx
@@ -5,7 +5,7 @@ import dynamic from 'next/dynamic';
 
 export default function DynamicMap(mapProps) {
   const Map = useMemo(() => dynamic(
-    () => import('../components/MapContainer'),
+    () => import('@/components/map/MapContainer'),
     {
       loading: () => <p>A map is loading...</p>,
       ssr: false
