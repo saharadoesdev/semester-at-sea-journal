88155893d74cb81f1cb7e653907bfa99bb8aa780
saharadoesdev <smsmo@att.net>
2025-08-22 12:37:44 -0500

    fetch only approved messages from database

--- src/app/message-wall/page.js
+++ src/app/message-wall/page.js
@@ -1,15 +1,13 @@
 import { createClient } from "@/utils/supabase/server";
 import MessageWallDisplay from "@/components/message-wall/MessageWallDisplay";
-import styles from "../page.module.css";
 
 export default async function MessageWallPage() {
   const supabase = await createClient();
-  const { data: messages, error } = await supabase.from("Messages").select().order('created_at', { ascending: false });
+  const { data: messages, error } = await supabase
+    .from("Messages")
+    .select()
+    .eq("status", "approved")
+    .order("created_at", { ascending: false });
 
-  return (
-    // <div className={styles.page}>
-    //   <h1 style={{ marginTop: "500px" }}>Message Wall</h1>
-      <MessageWallDisplay messages={messages || []} />
-    // </div>
-  );
+  return <MessageWallDisplay messages={messages || []} />;
 }
