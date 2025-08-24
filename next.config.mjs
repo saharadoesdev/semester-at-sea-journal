86c763083bbc7f78ab6ca2f58dc7ad116d941aa9
saharadoesdev <smsmo@att.net>
2025-08-23 22:28:59 -0500

    add redirect for /country to /route

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverActions: {
-            bodySizeLimit: '30mb',
-        },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "30mb",
     },
+  },
+  async redirects() {
+    return [
+      {
+        source: "/country",
+        destination: "/route",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
