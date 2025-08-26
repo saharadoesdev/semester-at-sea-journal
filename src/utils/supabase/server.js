249a60e563fef5add0656b6dd0d40d2296cf450e
saharadoesdev <smsmo@att.net>
2025-08-26 01:29:42 -0500

    remove cookies() from server.js to enable static page generation by default

--- src/utils/supabase/server.js
+++ src/utils/supabase/server.js
@@ -1,27 +1,19 @@
 import { createServerClient } from '@supabase/ssr'
-import { cookies } from 'next/headers'
 
-export async function createClient() {
-  const cookieStore = await cookies()
-
+// provides a dummy cookie handler to satisfy library's requirements
+// without calling any dynamic functions (for static pages)
+export function createClient() {
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     {
       cookies: {
+        // A no-op cookie handler for static pages
         getAll() {
-          return cookieStore.getAll()
+          return []
         },
         setAll(cookiesToSet) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            )
-          } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
+          // No-op
         },
       },
     }
